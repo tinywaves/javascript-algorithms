b2831d5db8ea88d915652ef76cf7330581b3c4c6
tinyRipple <zheng.dong.hui@foxmail.com>
2021-09-11 17:29:25 +0800

    217.ContainsDuplicate

---
+++ LeetCodeSolutions/src/ContainsDuplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        return true
+      }
+    }
+  }
+  return false
+};
