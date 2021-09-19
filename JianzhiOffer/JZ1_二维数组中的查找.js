d731f1fa4170f82cca58e3c9fe7f60386bdbc738
tinyRipple <zheng.dong.hui@foxmail.com>
2021-09-19 16:28:16 +0800

    [JianzhiOffer]JZ1

---
+++ JianzhiOffer/JZ1_二维数组中的查找.js
@@ -0,0 +1,13 @@
+function Find(target, array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      if (array[i][j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+module.exports = {
+  Find: Find,
+};
